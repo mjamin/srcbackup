61b2b613e49eb2e26e1a5f72f1e8ed73da9511ed
Marcel Jamin <marcel.jamin@trevios.com>
2015-07-17 13:14:11 +0200

    chore(*): adds scaffolding for first tests (derived from gitignore documentation)

---
+++ src/GitIgnorer.Tests/GitIgnoreParserTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Xunit;
+
+namespace GitIgnorer.Tests
+{
+    public class GitIgnoreParserTests
+    {
+        [Fact]
+        void ShouldIgnoreBlankLines()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldIgnoreLinesStartingWithHash()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldNotIgnoreLinesStartingWithEscapedHash()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldNotTrimEscapedTrailingSpaces()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldNegatePatternsStartingWithExclamationMark()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldNegatePatternsStartingWithEscapedExclamationMark()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldOnlyMatchDirectoriesIfPatternEndsWithSlash()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldCheckForMatchInPathRelativeToGitIgnoreIfPatternDoesNotContainSlash()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldNotMatchSlashesThroughWildcardsIfPatternContainsSlash()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldTreatLeadingSlashAsBeginningOfPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldTreatLeadingDoubleAsteriskFollowedBySlashAsMatchForAllDirectories()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldTreatTrailingSlashFollowedByDoubleAsteriskAsMatchForAllFilesWithin()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        void ShouldTreatSlashFollowedByByDoubleAsteriskFollowedBySlashAsMatchForZeroOrMoreDirectories()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
