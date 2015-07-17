5a7c3c759216e89e001be446134617ae40105b72
Marcel Jamin <marcel.jamin@trevios.com>
2015-07-17 13:13:13 +0200

    chore(*): adds solution with basic project structure

---
+++ src/GitIgnorer/GitIgnoreCompiler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIgnorer
+{
+    public class GitIgnoreCompiler
+    {
+        public GitIgnoreCompiler()
+        {
+        }
+
+        public GitIgnore Compile(GitIgnoreParseResult result)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/GitIgnorer/GitIgnoreParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitIgnorer
+{
+    public class GitIgnoreParser
+    {
+        public GitIgnoreParser()
+        {
+        }
+
+        public GitIgnoreParseResult Parse(string contents)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
