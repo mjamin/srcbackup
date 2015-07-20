d36aa33a38230deef3d4c590c9ba4700f7ad1abe
Marcel Jamin <marcel.jamin@trevios.com>
2015-07-20 10:32:21 +0200

    chore(*): adds scaffolding for GitIgnoreCompiler tests

---
+++ src/GitIgnorer.Tests/GitIgnoreCompilerTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Xunit;
+
+namespace GitIgnorer.Tests
+{
+    public class GitIgnoreCompilerTests
+    {
+        [Fact]
+        public void ShouldParseAndCompileExistingFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldThrowOnNonExistingFiles()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagNegated()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagRooted()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagMatchFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagMatchDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagMatchPathNameRelative()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagMatchInAllDirectories()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagMatchInsideDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagMatchZeroOrMoreDirectories()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ShouldCompilePatternsWithFlagWildcardsDoNotMatchSlashes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
