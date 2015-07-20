f5631312ea1da3b4f62e3ed14ee1d898260ade74
Marcel Jamin <marcel.jamin@trevios.com>
2015-07-20 12:17:42 +0200

    feat(*): implements GitIgnoreCompiler tests

--- src/GitIgnorer.Tests/GitIgnoreCompilerTests.cs
+++ src/GitIgnorer.Tests/GitIgnoreCompilerTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
 using Xunit;
 
 namespace GitIgnorer.Tests
@@ -8,67 +10,210 @@ namespace GitIgnorer.Tests
         [Fact]
         public void ShouldParseAndCompileExistingFiles()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var compiler = new GitIgnoreCompiler(new MockFileSystem().WithEmptyGitIgnore());
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.NotNull(result);
         }
 
         [Fact]
         public void ShouldThrowOnNonExistingFiles()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var compiler = new GitIgnoreCompiler(new MockFileSystem());
+
+            // Act / Assert
+            Assert.Throws<FileNotFoundException>(() =>
+            {
+                compiler.Compile(".gitignore");
+            });
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagNegated()
+        public void ShouldCompileNegatedPatterns()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("!foo.txt")
+                .WithFile("/foo.txt");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.False(result.Ignores("/foo.txt"));
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagRooted()
+        public void ShouldCompileRootedPatterns()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("/foo.txt")
+                .WithFiles("/bar/foo.txt", "/foo.txt");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/foo.txt"));
+            Assert.False(result.Ignores("/bar/foo.txt"));
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagMatchFile()
+        public void ShouldIgnoreMatchedFiles()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("foo.txt");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/foo.txt"));
+            Assert.True(result.Ignores("/bar/foo.txt"));
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagMatchDirectory()
+        public void ShouldIgnoreMatchedDirectories()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("foo/")
+                .WithDirectory("/foo")
+                .WithFile("/bar/foo");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/foo"));
+            Assert.False(result.Ignores("/bar/foo"));
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagMatchPathNameRelative()
+        public void ShouldIgnoreAllMatchingRelativePathsIfPatternDoesNotContainSlash()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("foo")
+                .WithFiles("/bar/baz/foo/barf.txt", "/bar/foo.txt", "/foo.txt");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/bar/baz/foo/barf.txt"));
+            Assert.True(result.Ignores("/bar/foo.txt"));
+            Assert.True(result.Ignores("/foo.txt"));
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagMatchInAllDirectories()
+        public void ShouldIgnoreMatchesInAllDirectories()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("**/foo/bar")
+                .WithFiles("/baz/foo/bar", "/foo/bar");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/baz/foo/bar"));
+            Assert.True(result.Ignores("/foo/bar"));
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagMatchInsideDirectory()
+        public void ShouldIgnoreEverythingInMatchedDirectory()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("abc/**")
+                .WithFiles("/abc/foo/bar.txt", "/abc/barf.txt");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/abc/foo/bar.txt"));
+            Assert.True(result.Ignores("/abc/barf.txt"));
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagMatchZeroOrMoreDirectories()
+        public void ShouldCompilePatternsFlaggedMatchZeroOrMoreDirectories()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("a/**/b")
+                .WithFiles("/a/b", "/a/x/b", "/a/x/y/b");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/a/b"));
+            Assert.True(result.Ignores("/a/x/b"));
+            Assert.True(result.Ignores("/a/x/y/b"));
         }
 
         [Fact]
-        public void ShouldCompilePatternsWithFlagWildcardsDoNotMatchSlashes()
+        public void ShouldApplyWildcardOnlyToFilenameIfPatternContainsSlash()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("Documentation/*.html")
+                .WithFiles("/Documentation/git.html", "/Documentation/ppc/ppc.html", "/tools/perf/Documentation/perf.html");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/Documentation/git.html"));
+            Assert.False(result.Ignores("/Documentation/ppc/ppc.html"));
+            Assert.False(result.Ignores("/tools/perf/Documentation/perf.html"));
+        }
+
+        [Fact]
+        public void ShouldMatchPatternsContainingAsteriskWildcard()
+        {
+            // Arrange
+            var fileSystem = new MockFileSystem()
+                .WithGitIgnoreContaining("*.txt")
+                .WithFiles("/foo/bar/baz.txt", "/foo/barf.txt", "/foo.txt");
+
+            var compiler = new GitIgnoreCompiler(fileSystem);
+
+            // Act
+            var result = compiler.Compile(".gitignore");
+
+            // Assert
+            Assert.True(result.Ignores("/foo/bar/baz.txt"));
+            Assert.True(result.Ignores("/foo/barf.txt"));
+            Assert.True(result.Ignores("/foo.txt"));
         }
     }
 }
---
+++ src/GitIgnorer.Tests/MockFileSystemExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+
+namespace GitIgnorer.Tests
+{
+    public static class MockFileSystemExtensions
+    {
+        public static MockFileSystem WithEmptyGitIgnore(this MockFileSystem mockFileSystem)
+        {
+            mockFileSystem.AddFile(".gitignore", new MockFileData(""));
+            return mockFileSystem;
+        }
+
+        public static MockFileSystem WithGitIgnoreContaining(this MockFileSystem mockFileSystem, string contents)
+        {
+            mockFileSystem.AddFile(".gitignore", new MockFileData(contents));
+            return mockFileSystem;
+        }
+
+        public static MockFileSystem WithDirectory(this MockFileSystem mockFileSystem, string path)
+        {
+            mockFileSystem.AddDirectory(path);
+            return mockFileSystem;
+        }
+
+        public static MockFileSystem WithFile(this MockFileSystem mockFileSystem, string path)
+        {
+            mockFileSystem.AddFile(path, new MockFileData(""));
+            return mockFileSystem;
+        }
+
+        public static MockFileSystem WithFiles(this MockFileSystem mockFileSystem, params string[] paths)
+        {
+            foreach (var path in paths)
+            {
+                mockFileSystem.AddFile(path, new MockFileData(""));
+            }
+
+            return mockFileSystem;
+        }
+    }
+}
